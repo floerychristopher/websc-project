f7d940adf868c65d3f4c9c12e09f8626aaaa40de
floerychristopher <floery.christopher@gmail.com>
2024-04-09 12:33:56 +0200

    HTML Boilerplate created

---
+++ frontend/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webscrpting Project</title>
+</head>
+<body>
+    <h1>Webscripting Project</h1>
+</body>
+</html>
